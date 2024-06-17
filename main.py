fa5949ca99ad8d26586abd096d918572155f7433
FestiveDucky <66172018+FestiveDucky@users.noreply.github.com>
2024-06-16 20:39:33 -0400

    Added tabs for graphs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from constants import *
 from simulation import Simulation
 
 # TODO
-# add saving to file and loading from file and add tabs for graphs
+# add saving to file and loading from file
 # Add button to swap into simulation mode which prevents graph editing
 # create vehicles with start locations and destinations
 # Write A* for vehicle pathing
@@ -21,6 +21,11 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
 
     s = Simulation()
+    s.loadGraphs()
+
+    # TODO later change this to a number saved in the file
+    graph_index = 0
+
     executing = True
     update = True
     # Starting the game loop
@@ -34,30 +39,30 @@ if __name__ == '__main__':
                 executing = False
             elif e.type == pygame.MOUSEBUTTONDOWN:
                 # First prioritize clicks on text boxes
-                textboxes_clicked = s.getGraph().getTextBoxesClicked(e.pos)
+                textboxes_clicked = s.getGraph(graph_index).getTextBoxesClicked(e.pos)
                 if len(textboxes_clicked) != 0:
                     textboxes_clicked[0].typing(True)
                 else:
                     # Gets the points that the mouse clicked
-                    points_clicked = s.getGraph().getPointsClicked(e.pos)
+                    points_clicked = s.getGraph(graph_index).getPointsClicked(e.pos)
 
                     if len(points_clicked) == 0:
-                        s.getGraph().addPoint(e.pos)
-                        s.getGraph().clearSelectedPoints()
+                        s.getGraph(graph_index).addPoint(e.pos)
+                        s.getGraph(graph_index).clearSelectedPoints()
                     else:
-                        s.getGraph().setMovingPoint(points_clicked[0])
+                        s.getGraph(graph_index).setMovingPoint(points_clicked[0])
 
                 update = True
             elif e.type == pygame.MOUSEMOTION:
-                update = s.getGraph().movePoint(e.pos)
+                update = s.getGraph(graph_index).movePoint(e.pos)
             elif e.type == pygame.MOUSEBUTTONUP:
-                s.getGraph().setMovingPoint(None)
+                s.getGraph(graph_index).setMovingPoint(None)
                 update = True
             elif e.type == pygame.KEYDOWN:
                 # Remove a point
                 if e.key == pygame.K_r:
                     mouse_position = pygame.mouse.get_pos()
-                    s.getGraph().removePoint(mouse_position)
+                    s.getGraph(graph_index).removePoint(mouse_position)
 
                     update = True
                 # Manual update
@@ -65,19 +70,26 @@ if __name__ == '__main__':
                     update = True
                 # Select a point
                 elif e.key == pygame.K_s:
-                    s.getGraph().selectPoint(pygame.mouse.get_pos())
+                    s.getGraph(graph_index).selectPoint(pygame.mouse.get_pos())
                     update = True
                 # Clear selected points
                 elif e.key == pygame.K_ESCAPE:
-                    s.getGraph().clearSelectedPoints()
+                    s.getGraph(graph_index).clearSelectedPoints()
                     update = True
                 # Adds an edge
                 elif e.key == pygame.K_e:
-                    s.getGraph().addEdge()
+                    s.getGraph(graph_index).addEdge()
                     update = True
                 # Deletes the edge between the selected points
                 elif e.key == pygame.K_d:
-                    s.getGraph().deleteEdge()
+                    s.getGraph(graph_index).deleteEdge()
+                    update = True
+                elif e.key == pygame.K_LEFT:
+                    graph_index -= 1
+                    graph_index = max(0, graph_index)
+                    update = True
+                elif e.key == pygame.K_RIGHT:
+                    graph_index += 1
                     update = True
 
         s.tick()
@@ -86,9 +98,9 @@ if __name__ == '__main__':
         if update:
             display.fill((14, 25, 36))
 
-            s.getGraph().drawEdges()
-            s.getGraph().updateTextBoxes()
-            s.getGraph().updatePoints()
+            s.getGraph(graph_index).drawEdges()
+            s.getGraph(graph_index).updateTextBoxes()
+            s.getGraph(graph_index).updatePoints()
 
             update = False
 
--- simulation.py
+++ simulation.py
@@ -3,10 +3,18 @@ from graph import UndirectedGraph
 
 class Simulation:
     def __init__(self):
-        self.graph = UndirectedGraph()
+        self.graphs = [UndirectedGraph()]
 
-    def getGraph(self):
-        return self.graph
+    def getGraph(self, index):
+        if index >= len(self.graphs):
+            self.graphs.append(UndirectedGraph())
+        return self.graphs[index]
+
+    def saveGraphs(self):
+        pass
+
+    def loadGraphs(self):
+        pass
 
     def tick(self):
         pass
